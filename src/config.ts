86658d5ead123f6f19a2b2fe0f842d3d9889ccc3
MeltedMint <meltedmint.liquid.purify@gmail.com>
2024-11-06 20:45:48 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type {
   LicenseConfig,
-  NovelNavBarConfig,
   NavBarConfig,
+  NovelNavBarConfig,
   ProfileConfig,
   SiteConfig,
 } from './types/config'
@@ -46,16 +46,16 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/melted-mint/fuwari-edited',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: '',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Melted_Mint',
+  bio: 'Hello!',
   links: [
     {
       name: 'Twitter',
@@ -72,7 +72,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/melted-mint/fuwari-edited',
     },
   ],
 }
@@ -91,7 +91,7 @@ export const NovelnavBarConfig: NovelNavBarConfig = {
     NovelLinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/melted-mint',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/melted-mint/fuwari-edited',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
