f6c7661b7186f419be8cd534793599c58def9cf3
Melted_Mint <meltedmint.liquid.purify@gmail.com>
2025-02-21 00:21:01 +0900

    backup

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,121 +20,123 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://meltedmint.com",
-  base: "/",
-  output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
-  trailingSlash: "always",
+    site: "https://meltedmint.com",
+    base: "/",
+    output: "static",
+    adapter: node({
+        mode: "standalone",
+    }),
+    trailingSlash: "always",
 
-  integrations: [
-    tailwind({
-        nesting: true,
-    }),
-    mdx(),
-    swup({
-      theme: false,
-      animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
-      // the default value `transition-` cause transition delay
-      // when the Tailwind class `transition-all` is used
-      containers: ["main", "#toc", "#sidebar"],
-      smoothScrolling: true,
-      cache: true,
-      preload: true,
-      accessibility: true,
-      updateHead: true,
-      updateBodyClass: false,
-      globalInstance: true,
-    }),
-    icon({
-      include: {
-        "preprocess: vitePreprocess(),": ["*"],
-        "fa6-brands": ["*"],
-        "fa6-regular": ["*"],
-        "fa6-solid": ["*"],
-      },
-    }),
-    svelte(),
-    sitemap(),
-    Compress({
-      CSS: false,
-      Image: false,
-      Action: {
-        Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
-      },
-    }),
-  ],
-
-  markdown: {
-    remarkPlugins: [
-      remarkMath,
-      remarkReadingTime,
-      remarkExcerpt,
-      remarkGithubAdmonitionsToDirectives,
-      remarkDirective,
-      remarkSectionize,
-      parseDirectiveNode,
-    ],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeSlug,
-      [
-        rehypeComponents,
-        {
-          components: {
-            github: GithubCardComponent,
-            note: (x, y) => AdmonitionComponent(x, y, "note"),
-            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-            important: (x, y) => AdmonitionComponent(x, y, "important"),
-            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
-            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
-          },
-        },
-      ],
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          properties: {
-            className: ["anchor"],
-          },
-          content: {
-            type: "element",
-            tagName: "span",
-            properties: {
-              className: ["anchor-icon"],
-              "data-pagefind-ignore": true,
+    integrations: [
+        tailwind({
+            nesting: true,
+        }),
+        mdx(),
+        swup({
+            theme: false,
+            animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
+            // the default value `transition-` cause transition delay
+            // when the Tailwind class `transition-all` is used
+            containers: ["main", "#toc", "#sidebar"],
+            smoothScrolling: true,
+            cache: true,
+            preload: true,
+            accessibility: true,
+            updateHead: true,
+            updateBodyClass: false,
+            globalInstance: true,
+        }),
+        icon({
+            include: {
+                "preprocess: vitePreprocess(),": ["*"],
+                "fa6-brands": ["*"],
+                "fa6-regular": ["*"],
+                "fa6-solid": ["*"],
             },
-            children: [
-              {
-                type: "text",
-                value: "#",
-              },
-            ],
-          },
-        },
-      ],
+        }),
+        svelte(),
+        sitemap(),
+        Compress({
+            CSS: false,
+            Image: false,
+            Action: {
+                Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
+            },
+        }),
+        pagefind(),
     ],
-  },
 
-  vite: {
-    build: {
-      rollupOptions: {
-        onwarn(warning, warn) {
-          // temporarily suppress this warning
-          if (
-            warning.message.includes("is dynamically imported by") &&
-            warning.message.includes("but also statically imported by")
-          ) {
-            return;
-          }
-          warn(warning);
-        },
-      },
+    markdown: {
+        remarkPlugins: [
+            remarkMath,
+            remarkReadingTime,
+            remarkExcerpt,
+            remarkGithubAdmonitionsToDirectives,
+            remarkDirective,
+            remarkSectionize,
+            parseDirectiveNode,
+        ],
+        rehypePlugins: [
+            rehypeKatex,
+            rehypeSlug,
+            [
+                rehypeComponents,
+                {
+                    components: {
+                        github: GithubCardComponent,
+                        note: (x, y) => AdmonitionComponent(x, y, "note"),
+                        tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+                        important: (x, y) => AdmonitionComponent(x, y, "important"),
+                        caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+                        warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+                    },
+                },
+            ],
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: "append",
+                    properties: {
+                        className: ["anchor"],
+                    },
+                    content: {
+                        type: "element",
+                        tagName: "span",
+                        properties: {
+                            className: ["anchor-icon"],
+                            "data-pagefind-ignore": true,
+                        },
+                        children: [
+                            {
+                                type: "text",
+                                value: "#",
+                            },
+                        ],
+                    },
+                },
+            ],
+        ],
     },
-  },
+
+    vite: {
+        build: {
+            rollupOptions: {
+                onwarn(warning, warn) {
+                    // temporarily suppress this warning
+                    if (
+                        warning.message.includes("is dynamically imported by") &&
+                        warning.message.includes("but also statically imported by")
+                    ) {
+                        return;
+                    }
+                    warn(warning);
+                },
+            },
+        },
+    },
 });
